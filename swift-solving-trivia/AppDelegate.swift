6e541bc821f79fc1e1fc0bfe97a00d35df2bb35b
JohnPH75 <johnhussain75@gmail.com>
2016-07-15 10:55:34 -0400

    swift trivia

--- swift-solving-trivia/AppDelegate.swift
+++ swift-solving-trivia/AppDelegate.swift
@@ -17,6 +17,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
      write your function here
      
      */
+    
+    func solveTrivia (statesAndCapitals: [String:String]) -> String {
+        
+        for (state, capital) in statesAndCapitals {
+            
+            var characterCount = 0
+            
+            for stateCharacter in state.lowercaseString.characters {
+                
+                let correctState = String(stateCharacter)
+                
+                if capital.containsString(correctState) {
+                    break;
+                    
+                }
+                
+                characterCount += 1
+            }
+            
+            if characterCount == state.characters.count {
+                
+                print("*****\n\n \(state) is the only state whose capital contains none of the characters as the state name. \n\n******")
+                return state
+            }
+        }
+        
+        return "state not found"
+    }
 
 }
 
